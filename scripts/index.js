e921e5149cdde2dd9fc26e4f717022f8b5acb193
mikaem <mkmanaligod@gmail.com>
2023-03-01 13:48:13 -0800

    implementing slide function

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,35 @@
-function swiper(){
+// function swiper(){
+//     $(".carousel").swipe({
+//     swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+//         if (direction == 'left') $(this).carousel('next');
+//         if (direction == 'right') $(this).carousel('prev');
+
+//         if (distance == '1px') $(this).carousel('next');
+//         if (distance == '1px') $(this).carousel('prev');
+
+
+//     },
+//     allowPageScroll:"vertical"
+//     });
+// }
+
+// swiper();
+
+if (Modernizr.touch) {
     $(".carousel").swipe({
-    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-        if (direction == 'left') $(this).carousel('next');
-        if (direction == 'right') $(this).carousel('prev');
-    },
-    allowPageScroll:"vertical"
+        swipe: function (
+            event,
+            direction,
+            distance,
+            duration,
+            fingerCount,
+            fingerData
+        ) {
+            if (direction == "left") $(this).carousel("next");
+            if (direction == "right") $(this).carousel("prev");
+        },
+        allowPageScroll: "vertical"
     });
+} else {
+    $(".alert").removeClass("hidden");
 }
-
-swiper();
-
-
-
