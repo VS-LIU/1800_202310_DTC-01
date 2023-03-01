913dd11a328dc23385795258e3f522fe7d490966
mikaem <mkmanaligod@gmail.com>
2023-03-01 13:57:48 -0800

    adjusted carousel

--- scripts/index.js
+++ scripts/index.js
@@ -14,22 +14,3 @@
 // }
 
 // swiper();
-
-if (Modernizr.touch) {
-    $(".carousel").swipe({
-        swipe: function (
-            event,
-            direction,
-            distance,
-            duration,
-            fingerCount,
-            fingerData
-        ) {
-            if (direction == "left") $(this).carousel("next");
-            if (direction == "right") $(this).carousel("prev");
-        },
-        allowPageScroll: "vertical"
-    });
-} else {
-    $(".alert").removeClass("hidden");
-}
