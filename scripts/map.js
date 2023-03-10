37bccae531c77bbc2b4115784e153a8ccd2eb209
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-03-10 12:05:35 -0800

    Modify map.js

--- scripts/map.js
+++ scripts/map.js
@@ -3,199 +3,197 @@
 function addMapPins() {
 
     var mapReference = db.collection("map") //keep name???
-
-    // Template to add objects from here
-    // mapReference.add({
-    //     name: "",
-    //     address: "",
-    //     city: "",
-    //     province: "",
-    //     postcode: "",
-    //     latitude: 0,
-    //     longitude: 0,
-    //     last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
-    // });
+    mapReference.add({
+        address: "Jim Pattison Pavilion, 899 W 12th Ave",
+        city: "Vancouver",
+        latitude: 49.25666564,
+        longitude: "-123.120499518",
+        name: "Vancouver General Hospital",
+        postcode: " V5Z 1M9",
+        province: "British Columbia",
+        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+    });
 }
 
 function showMap() {
-    // Defines basic mapbox data
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhbWNoZW4zIiwiYSI6ImNsMGZyNWRtZzB2angzanBjcHVkNTQ2YncifQ.fTdfEXaQ70WoIFLZ2QaRmQ';
-    const map = new mapboxgl.Map({
-        container: 'map', // Container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // Styling URL
-        center: [-122.964274, 49.236082], // Starting position
-        zoom: 8 // Starting zoom
-    });
+            // Defines basic mapbox data
+            mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhbWNoZW4zIiwiYSI6ImNsMGZyNWRtZzB2angzanBjcHVkNTQ2YncifQ.fTdfEXaQ70WoIFLZ2QaRmQ';
+            const map = new mapboxgl.Map({
+                container: 'map', // Container ID
+                style: 'mapbox://styles/mapbox/streets-v11', // Styling URL
+                center: [-122.964274, 49.236082], // Starting position
+                zoom: 8 // Starting zoom
+            });
 
-    // Add user controls to map
-    map.addControl(new mapboxgl.NavigationControl());
+            // Add user controls to map
+            map.addControl(new mapboxgl.NavigationControl());
 
-    // Adds map features
-    map.on('load', () => {
-        // Defines map pin icon for events
-        map.loadImage(
-            'https://cdn.iconscout.com/icon/free/png-256/pin-locate-marker-location-navigation-16-28668.png',
-            (error, image) => {
-                if (error) throw error;
+            // Adds map features
+            map.on('load', () => {
+                // Defines map pin icon for events
+                map.loadImage(
+                    'https://cdn.iconscout.com/icon/free/png-256/pin-locate-marker-location-navigation-16-28668.png',
+                    (error, image) => {
+                        if (error) throw error;
 
-                // Add the image to the map style.
-                map.addImage('eventpin', image); // Pin Icon
+                        // Add the image to the map style.
+                        map.addImage('eventpin', image); // Pin Icon
 
-                // READING information from "events" collection in Firestore
-                db.collection('map').get().then(allEvents => {
-                    const features = []; // Defines an empty array for information to be added to
+                        // READING information from "events" collection in Firestore
+                        db.collection('map').get().then(allEvents => {
+                            const features = []; // Defines an empty array for information to be added to
 
-                    allEvents.forEach(doc => {
-                        lat = doc.data().latitude;
-                        lng = doc.data().longitude;
-                        console.log(lat, lng);
-                        coordinates = [lng, lat];
-                        console.log(coordinates);
-                        // Coordinates
-                       pin_name = doc.data().name; // Event Name
+                            allEvents.forEach(doc => {
+                                lat = doc.data().latitude;
+                                lng = doc.data().longitude;
+                                console.log(lat, lng);
+                                coordinates = [lng, lat];
+                                console.log(coordinates);
+                                // Coordinates
+                                pin_name = doc.data().name; // Event Name
 
-                        // we dont have this...
+                                // we dont have this...
 
-                        // preview = doc.data().details; // Text Preview
-                        // img = doc.data().posterurl; // Image
-                        // url = doc.data().link; // URL
+                                // preview = doc.data().details; // Text Preview
+                                // img = doc.data().posterurl; // Image
+                                // url = doc.data().link; // URL
 
-                        // Pushes information into the features array
-                        features.push({
-                            'type': 'Feature',
-                            'properties': {
-                                'description': `<strong>${pin_name}</strong> <br> <a href="/map.html?id=${doc.id}" target="_blank" title="Opens in a new window">Read more</a>`
-                            },
-                            'geometry': {
-                                'type': 'Point',
-                                'coordinates': coordinates
-                            }
-                        });
-                    });
-
-                    // Adds features as a source to the map
-                    map.addSource('places', {
-                        'type': 'geojson',
-                        'data': {
-                            'type': 'FeatureCollection',
-                            'features': features
-                        }
-                    });
-
-                    // Creates a layer above the map displaying the pins
-                    map.addLayer({
-                        'id': 'places',
-                        'type': 'symbol',
-                        // source: 'places',
-                        'source': 'places',
-                        'layout': {
-                            'icon-image': 'eventpin', // Pin Icon
-                            'icon-size': 0.1, // Pin Size
-                            'icon-allow-overlap': true // Allows icons to overlap
-                        }
-                    });
-
-                    // Map On Click function that creates a popup, displaying previously defined information from "events" collection in Firestore
-                    map.on('click', 'places', (e) => {
-                        // Copy coordinates array.
-                        const coordinates = e.features[0].geometry.coordinates.slice();
-                        const description = e.features[0].properties.description;
-
-                        // Ensure that if the map is zoomed out such that multiple copies of the feature are visible, the popup appears over the copy being pointed to.
-                        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-                        }
-
-                        new mapboxgl.Popup()
-                            .setLngLat(coordinates)
-                            .setHTML(description)
-                            .addTo(map);
-                    });
-
-                    // Change the cursor to a pointer when the mouse is over the places layer.
-                    map.on('mouseenter', 'places', () => {
-                        map.getCanvas().style.cursor = 'pointer';
-                    });
-
-                    // Defaults cursor when not hovering over the places layer
-                    map.on('mouseleave', 'places', () => {
-                        map.getCanvas().style.cursor = '';
-                    });
-                });
-            }
-        );
-
-        // Add the image to the map style.
-        map.loadImage(
-            'https://cdn-icons-png.flaticon.com/512/61/61168.png',
-            (error, image) => {
-                if (error) throw error;
-
-                // Add the image to the map style with width and height values
-                map.addImage('userpin', image, { width: 10, height: 10 });
-
-                // Adds user's current location as a source to the map
-                navigator.geolocation.getCurrentPosition(position => {
-                    const userLocation = [position.coords.longitude, position.coords.latitude];
-                    console.log(userLocation);
-                    if (userLocation) {
-                        map.addSource('userLocation', {
-                            'type': 'geojson',
-                            'data': {
-                                'type': 'FeatureCollection',
-                                'features': [{
+                                // Pushes information into the features array
+                                features.push({
                                     'type': 'Feature',
+                                    'properties': {
+                                        'description': `<strong>${pin_name}</strong> <br> <a href="/map.html?id=${doc.id}" target="_blank" title="Opens in a new window">Read more</a>`
+                                    },
                                     'geometry': {
                                         'type': 'Point',
-                                        'coordinates': userLocation
-                                    },
-                                    'properties': {
-                                        'description': 'Your location'
+                                        'coordinates': coordinates
                                     }
-                                }]
-                            }
-                        });
+                                });
+                            });
 
-                        // Creates a layer above the map displaying the user's location
-                        map.addLayer({
-                            'id': 'userLocation',
-                            'type': 'symbol',
-                            'source': 'userLocation',
-                            'layout': {
-                                'icon-image': 'userpin', // Pin Icon
-                                'icon-size': 0.05, // Pin Size
-                                'icon-allow-overlap': true // Allows icons to overlap
-                            }
-                        });
+                            // Adds features as a source to the map
+                            map.addSource('places', {
+                                'type': 'geojson',
+                                'data': {
+                                    'type': 'FeatureCollection',
+                                    'features': features
+                                }
+                            });
 
-                        // Map On Click function that creates a popup displaying the user's location
-                        map.on('click', 'userLocation', (e) => {
-                            // Copy coordinates array.
-                            const coordinates = e.features[0].geometry.coordinates.slice();
-                            const description = e.features[0].properties.description;
+                            // Creates a layer above the map displaying the pins
+                            map.addLayer({
+                                'id': 'places',
+                                'type': 'symbol',
+                                // source: 'places',
+                                'source': 'places',
+                                'layout': {
+                                    'icon-image': 'eventpin', // Pin Icon
+                                    'icon-size': 0.1, // Pin Size
+                                    'icon-allow-overlap': true // Allows icons to overlap
+                                }
+                            });
 
-                            new mapboxgl.Popup()
-                                .setLngLat(coordinates)
-                                .setHTML(description)
-                                .addTo(map);
-                        });
+                            // Map On Click function that creates a popup, displaying previously defined information from "events" collection in Firestore
+                            map.on('click', 'places', (e) => {
+                                // Copy coordinates array.
+                                const coordinates = e.features[0].geometry.coordinates.slice();
+                                const description = e.features[0].properties.description;
 
-                        // Change the cursor to a pointer when the mouse is over the userLocation layer.
-                        map.on('mouseenter', 'userLocation', () => {
-                            map.getCanvas().style.cursor = 'pointer';
-                        });
+                                // Ensure that if the map is zoomed out such that multiple copies of the feature are visible, the popup appears over the copy being pointed to.
+                                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                                }
 
-                        // Defaults
-                        // Defaults cursor when not hovering over the userLocation layer
-                        map.on('mouseleave', 'userLocation', () => {
-                            map.getCanvas().style.cursor = '';
+                                new mapboxgl.Popup()
+                                    .setLngLat(coordinates)
+                                    .setHTML(description)
+                                    .addTo(map);
+                            });
+
+                            // Change the cursor to a pointer when the mouse is over the places layer.
+                            map.on('mouseenter', 'places', () => {
+                                map.getCanvas().style.cursor = 'pointer';
+                            });
+
+                            // Defaults cursor when not hovering over the places layer
+                            map.on('mouseleave', 'places', () => {
+                                map.getCanvas().style.cursor = '';
+                            });
                         });
                     }
-                });
-            }
-        );
-    });
-}
+                );
+
+                // Add the image to the map style.
+                map.loadImage(
+                    'https://cdn-icons-png.flaticon.com/512/61/61168.png',
+                    (error, image) => {
+                        if (error) throw error;
+
+                        // Add the image to the map style with width and height values
+                        map.addImage('userpin', image, { width: 10, height: 10 });
+
+                        // Adds user's current location as a source to the map
+                        navigator.geolocation.getCurrentPosition(position => {
+                            const userLocation = [position.coords.longitude, position.coords.latitude];
+                            console.log(userLocation);
+                            if (userLocation) {
+                                map.addSource('userLocation', {
+                                    'type': 'geojson',
+                                    'data': {
+                                        'type': 'FeatureCollection',
+                                        'features': [{
+                                            'type': 'Feature',
+                                            'geometry': {
+                                                'type': 'Point',
+                                                'coordinates': userLocation
+                                            },
+                                            'properties': {
+                                                'description': 'Your location'
+                                            }
+                                        }]
+                                    }
+                                });
+
+                                // Creates a layer above the map displaying the user's location
+                                map.addLayer({
+                                    'id': 'userLocation',
+                                    'type': 'symbol',
+                                    'source': 'userLocation',
+                                    'layout': {
+                                        'icon-image': 'userpin', // Pin Icon
+                                        'icon-size': 0.05, // Pin Size
+                                        'icon-allow-overlap': true // Allows icons to overlap
+                                    }
+                                });
+
+                                // Map On Click function that creates a popup displaying the user's location
+                                map.on('click', 'userLocation', (e) => {
+                                    // Copy coordinates array.
+                                    const coordinates = e.features[0].geometry.coordinates.slice();
+                                    const description = e.features[0].properties.description;
+
+                                    new mapboxgl.Popup()
+                                        .setLngLat(coordinates)
+                                        .setHTML(description)
+                                        .addTo(map);
+                                });
+
+                                // Change the cursor to a pointer when the mouse is over the userLocation layer.
+                                map.on('mouseenter', 'userLocation', () => {
+                                    map.getCanvas().style.cursor = 'pointer';
+                                });
+
+                                // Defaults
+                                // Defaults cursor when not hovering over the userLocation layer
+                                map.on('mouseleave', 'userLocation', () => {
+                                    map.getCanvas().style.cursor = '';
+                                });
+                            }
+                        });
+                    }
+                );
+            });
+        }
 
 // Call the function to display the map with the user's location and event pins
 showMap();
