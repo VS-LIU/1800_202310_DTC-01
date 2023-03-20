473015c223f4de68f2d0f0305c52d07ba205a303
Victor Liu <vctl95@gmail.com>
2023-03-19 21:35:36 -0700

    Add code

--- scripts/createpost.js
+++ scripts/createpost.js
@@ -1,66 +1,93 @@
-const storage = firebase.storage();
+// const storage = firebase.storage();
 
-function uploadPic(postDocID) {
-    console.log("inside uploadPic " + postDocID);
-    var storageRef = storage.ref("images/" + postDocID + ".jpg");
+// function uploadPic(postDocID) {
+//     console.log("inside uploadPic " + postDocID);
+//     var storageRef = storage.ref("images/" + postDocID + ".jpg");
 
-    storageRef.put(ImageFile)   //global variable ImageFile
+//     storageRef.put(ImageFile)   //global variable ImageFile
 
-        // AFTER .put() is done
-        .then(function () {
-            console.log('Uploaded to Cloud Storage.');
-            storageRef.getDownloadURL()
+//         // AFTER .put() is done
+//         .then(function () {
+//             console.log('Uploaded to Cloud Storage.');
+//             storageRef.getDownloadURL()
 
-                // AFTER .getDownloadURL is done
-                .then(function (url) { // Get URL of the uploaded file
-                    console.log("Got the download URL.");
-                    db.collection("posts").doc(postDocID).update({
-                        "image": url // Save the URL into users collection
-                    })
+//                 // AFTER .getDownloadURL is done
+//                 .then(function (url) { // Get URL of the uploaded file
+//                     console.log("Got the download URL.");
+//                     db.collection("posts").doc(postDocID).update({
+//                         "image": url // Save the URL into users collection
+//                     })
 
-                        // AFTER .update is done
-                        .then(function () {
-                            console.log('Added pic URL to Firestore.');
-                        })
-                })
-        })
-        .catch((error) => {
-            console.log("error uploading to cloud storage");
-        })
+//                         // AFTER .update is done
+//                         .then(function () {
+//                             console.log('Added pic URL to Firestore.');
+//                         })
+//                 })
+//         })
+//         .catch((error) => {
+//             console.log("error uploading to cloud storage");
+//         })
+// }
+
+
+// var ImageFile;
+// function listenFileSelect() {
+//     // listen for file selection
+//     var fileInput = document.getElementById("mypic-input"); // pointer #1
+//     const image = document.getElementById("mypic-goes-here"); // pointer #2
+
+//     // When a change happens to the File Chooser Input
+//     fileInput.addEventListener('change', function (e) {
+//         ImageFile = e.target.files[0];   //Global variable
+//         var blob = URL.createObjectURL(ImageFile);
+//         image.src = blob; // Display this image
+//     })
+// }
+// listenFileSelect();
+
+// function to save post id to user's document in database
+function saveNewPostID(userUID, postDocID){
+    db.collection("users")
+             .doc(userUID)
+             .update({
+                 posts: firebase.firestore.FieldValue.arrayUnion(postDocID)
+             })
+             .then(() =>
+                 console.log("Saved to user's document!"))
+             .catch((error) => {
+                 console.error("Error writing document: ", error);
+             });
 }
 
-
-var ImageFile;
-function listenFileSelect() {
-    // listen for file selection
-    var fileInput = document.getElementById("mypic-input"); // pointer #1
-    const image = document.getElementById("mypic-goes-here"); // pointer #2
-
-    // When a change happens to the File Chooser Input
-    fileInput.addEventListener('change', function (e) {
-        ImageFile = e.target.files[0];   //Global variable
-        var blob = URL.createObjectURL(ImageFile);
-        image.src = blob; // Display this image
-    })
-}
-listenFileSelect();
 
 function savePost() {
     alert("SAVE POST is triggered");
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             // User is signed in.
-            // Do something for the user here. 
-            var desc = document.getElementById("description").value;
+            // Define variables for value of each field 
+            var postTitle = document.getElementById("createTitle").value;
+            console.log("postTitle: " + postTitle);
+            var postLoc = document.getElementById("createLocation").value;
+            var postCatSelect = document.getElementById("createCategory");
+            var postCat = postCatSelect.options[postCatSelect.selectedIndex].text;
+            console.log("postCat: " + postCat);
+            var postDesc = document.getElementById("createDescription").value;
+            // Write post to database
             db.collection("posts").add({
                 owner: user.uid,
-                description: desc,
+                title: postTitle,
+                location: postLoc,
+                category: postCat,
+                description: postDesc,
                 last_updated: firebase.firestore.FieldValue
                     .serverTimestamp() //current system time
             }).then(doc => {
                 console.log("Post document added!");
                 console.log(doc.id);
-                uploadPic(doc.id);
+                // uploadPic(doc.id); ???
+                // Save post id to user's document in database
+                saveNewPostID(user.uid, doc.id);
             })
         } else {
             // No user is signed in.
