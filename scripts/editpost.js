1937cc8f0a7579a2991ab00dab27d0e9a5f7c8d1
Victor Liu <vctl95@gmail.com>
2023-03-19 21:40:38 -0700

    Create new script file for editting posts

---
+++ scripts/editpost.js
@@ -0,0 +1,7 @@
+// for editing posts
+// 1. get the post id from the URL
+// 2. get the post data from Firestore
+// 3. display the post data in the form
+// 4. listen for changes to the form
+// 5. save the changes to Firestore
+// 6. redirect to the post page with the new data
