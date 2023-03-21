e5f1014e6b891b458811ce8517a5b4d65d8bc88c
Emma Lee <jy.lee.emma@gmail.com>
2023-03-21 14:42:47 -0700

    Uncomment createpost

--- scripts/createpost.js
+++ scripts/createpost.js
@@ -1,49 +1,49 @@
 const storage = firebase.storage();
 
-//function uploadPic(postDocID) {
-// //    console.log("inside uploadPic " + postDocID);
-//  //   var storageRef = storage.ref("images/" + postDocID + ".jpg");
+function uploadPic(postDocID) {
+    console.log("inside uploadPic " + postDocID);
+    var storageRef = storage.ref("images/" + postDocID + ".jpg");
 
-//     storageRef.put(ImageFile)   //global variable ImageFile
+    storageRef.put(ImageFile)   //global variable ImageFile
 
-//         // AFTER .put() is done
-//         .then(function () {
-//             console.log('Uploaded to Cloud Storage.');
-//             storageRef.getDownloadURL()
+    AFTER.put() is done
+        .then(function () {
+            console.log('Uploaded to Cloud Storage.');
+            storageRef.getDownloadURL()
 
-//                 // AFTER .getDownloadURL is done
-//                 .then(function (url) { // Get URL of the uploaded file
-//                     console.log("Got the download URL.");
-//                     db.collection("posts").doc(postDocID).update({
-//                         "image": url // Save the URL into users collection
-//                     })
+            AFTER.getDownloadURL is done
+                .then(function (url) { // Get URL of the uploaded file
+                    console.log("Got the download URL.");
+                    db.collection("posts").doc(postDocID).update({
+                        "image": url // Save the URL into users collection
+                    })
 
-//                         // AFTER .update is done
-//                         .then(function () {
-//                             console.log('Added pic URL to Firestore.');
-//                         })
-//                 })
-//         })
-//         .catch((error) => {
-//             console.log("error uploading to cloud storage");
-//         })
-// }
+                    AFTER.update is done
+                        .then(function () {
+                            console.log('Added pic URL to Firestore.');
+                        })
+                })
+        })
+        .catch((error) => {
+            console.log("error uploading to cloud storage");
+        })
+}
 
 
-// var ImageFile;
-// function listenFileSelect() {
-//     // listen for file selection
-//     var fileInput = document.getElementById("mypic-input"); // pointer #1
-//     const image = document.getElementById("mypic-goes-here"); // pointer #2
+var ImageFile;
+function listenFileSelect() {
+listen for file selection
+var fileInput = document.getElementById("mypic-input"); // pointer #1
+    const image = document.getElementById("mypic-goes-here"); // pointer #2
 
-//     // When a change happens to the File Chooser Input
-//     fileInput.addEventListener('change', function (e) {
-//         ImageFile = e.target.files[0];   //Global variable
-//         var blob = URL.createObjectURL(ImageFile);
-//         image.src = blob; // Display this image
-//     })
-// }
-//listenFileSelect();
+ When a change happens to the File Chooser Input
+    fileInput.addEventListener('change', function (e) {
+        ImageFile = e.target.files[0];   //Global variable
+        var blob = URL.createObjectURL(ImageFile);
+        image.src = blob; // Display this image
+    })
+}
+listenFileSelect();
 
 function savePost() {
     alert("SAVE POST is triggered");
