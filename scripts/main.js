05d0d4cdb1891a2f0333f4f297830dee83a1f439
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-03-24 12:25:09 -0700

    modify main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,12 @@ function displayCardsDynamically(collection) {
             allPosts.forEach(doc => { //iterate through each doc
                 let title = doc.data().title;
                 console.log("allPosts: " + doc.data().title)
-                let imageURL = doc.data().image;
+                let imageURL = doc.get("image");
                 var docID = doc.id;
                 let listingCard = cardTemplate.content.cloneNode(true);
 
                 //update title and text and image etc.
-                listingCard.querySelector('.cb-card-img').src = `${imageURL}`;
+                listingCard.querySelector('.cb-card-img').src = imageURL;
                 listingCard.querySelector('.cb-card-title').innerHTML = title;
                 listingCard.querySelector('.click-card').setAttribute('onclick', `location.href='./viewListing.html?docID=${docID}'`);
                 console.log("TEST #2!");
