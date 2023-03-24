667a1b51a5e62e3ba3200b1f59eb8fe6947e02aa
Victor Liu <vctl95@gmail.com>
2023-03-24 00:50:06 -0700

    Add code

--- scripts/myListingsV.js
+++ scripts/myListingsV.js
@@ -49,35 +49,37 @@ function displayMyPostCard(doc) {
 }
 
 
-// //function that uploads the listings from the database to the page
+//function that uploads the listings from the database to the page
 // function displayCardsDynamically(collection) {
-//     let cardTemplate = document.getElementById("listingTemplate");
+//     let cardTemplate = document.getElementById("postCardTemplate");
 //     console.log("hello")
 
 //     db.collection("posts").orderBy("last_updated", "desc").get() //get all docs from collection and orders them by last_updated
 //         .then(allListings => {
-//             //order by last_updated in descending order
-
-
 //             // console.log("allListings: " + allListings.docs)
 //             allListings.forEach(doc => { //iterate thru each doc
 //                 var title = doc.data().title;
 //                 console.log("allListings: " + doc.data().title)
 
+//                 var image = doc.data().image;
+//                 console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", image)
 //                 var description = doc.data().description;
 //                 var docID = doc.id;
 //                 let newcard = cardTemplate.content.cloneNode(true);
 
 //                 //update title and text and image etc.
 //                 newcard.querySelector('.card-title').innerHTML = title;
-//                 newcard.querySelector('.card-text').innerHTML = description;
-//                 newcard.querySelector('.card-link').href = `./viewListing.html?${docID}`; //Example: ./viewpost.html?NV01
+//                 newcard.querySelector('.card-description').innerHTML = description;
+//                 //update image
+//                 // newcard.querySelector('.card-image').src = `./images/${docID}.jpg`;
+//                 newcard.querySelector('.card-image').src = image;
+//                 // update link
+//                 newcard.querySelector('.card-link').href = `./viewListing.html?docID=${docID}`; //Example: ./viewpost.html?NV01
 
 //                 // //Finally done modifying newcard
-//                 // //attach to gallery, Example: "hikes-go-here"
-//                 document.getElementById("posts-go-here").appendChild(newcard);
+//                 // document.getElementById("posts-go-here").appendChild(newcard);
+//                 document.getElementById("posts-go-here").prepend(newcard);
 
-//                 //i++;   //Optional: iterate variable to serve as unique ID
 //             })
 //         })
 // }
