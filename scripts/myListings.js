655246c46e69f13c9ac58cf942c9d26b507751fd
mikaem <mkmanaligod@gmail.com>
2023-03-31 11:55:16 -0700

    reverted myListings back to original

--- scripts/myListings.js
+++ scripts/myListings.js
@@ -51,3 +51,40 @@ function displayMyPostCard(doc) {
     newcard.querySelector('.click-card').setAttribute('onclick', `location.href='./viewListing.html?docID=${docID}'`);
     document.getElementById("myposts-go-here").prepend(newcard);
 }
+
+
+//function that uploads the listings from the database to the page
+// function displayCardsDynamically(collection) {
+//     let cardTemplate = document.getElementById("postCardTemplate");
+//     console.log("hello")
+
+//     db.collection("posts").orderBy("last_updated", "desc").get() //get all docs from collection and orders them by last_updated
+//         .then(allListings => {
+//             // console.log("allListings: " + allListings.docs)
+//             allListings.forEach(doc => { //iterate thru each doc
+//                 var title = doc.data().title;
+//                 console.log("allListings: " + doc.data().title)
+
+//                 var image = doc.data().image;
+//                 console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", image)
+//                 var description = doc.data().description;
+//                 var docID = doc.id;
+//                 let newcard = cardTemplate.content.cloneNode(true);
+
+//                 //update title and text and image etc.
+//                 newcard.querySelector('.card-title').innerHTML = title;
+//                 newcard.querySelector('.card-description').innerHTML = description;
+//                 //update image
+//                 // newcard.querySelector('.card-image').src = `./images/${docID}.jpg`;
+//                 newcard.querySelector('.card-image').src = image;
+//                 // update link
+//                 newcard.querySelector('.card-link').href = `./viewListing.html?docID=${docID}`; //Example: ./viewpost.html?NV01
+
+//                 // //Finally done modifying newcard
+//                 // document.getElementById("posts-go-here").appendChild(newcard);
+//                 document.getElementById("posts-go-here").prepend(newcard);
+
+//             })
+//         })
+// }
+// displayCardsDynamically("posts");
