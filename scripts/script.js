7d52b70d2fa45eea010e7406f4049d3d99d6e9ca
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-04-02 13:57:13 -0700

    Try add logout function

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+  firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      console.log("logging out user");
+    }).catch((error) => {
+      // An error happened.
+    });
+}
+
+logout();
