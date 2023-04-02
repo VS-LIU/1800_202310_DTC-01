9676853111b5d489f877b1d459639af4e6ffeba1
Victor Liu <vctl95@gmail.com>
2023-04-02 02:23:29 -0700

    Fix communityboard image not displaying

--- scripts/communityboard.js
+++ scripts/communityboard.js
@@ -7,18 +7,20 @@ function displayCardsDynamically(collection) {
     db.collection("posts").orderBy("last_updated").get() 
         .then(allListings => {
             allListings.forEach(doc => { 
-                var title = doc.data().title;
+                // var title = doc.data().title;
+                var title = doc.get("title");
+                var image = doc.get("image");
                 console.log("allListings: " + doc.data().title)
 
-                var description = doc.data().description;
+                // var description = doc.data().description;
                 var docID = doc.id;
                 let newcard = cardTemplate.content.cloneNode(true);
 
                 newcard.querySelector('.card-title').innerHTML = title;
                 // newcard.querySelector('.card-text').innerHTML = description;
-                newcard.querySelector('.card-image').src = `./images/${docID}.jpg`;
-                newcard.querySelector('.card-link').href = `./viewListing.html?docID=${docID}`; //Example: ./viewpost.html?NV01
-
+                newcard.querySelector('.card-image').src = image;
+                // newcard.querySelector('.card-link').href = `./viewListing.html?docID=${docID}`; //Example: ./viewpost.html?NV01
+                newcard.querySelector('.click-card').setAttribute('onclick', `location.href='./viewListing.html?docID=${docID}'`);
                 document.getElementById("posts-go-here").appendChild(newcard);
 
             })
