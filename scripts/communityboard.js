56ebe5b26d019152570b1870858eca33eb6d3302
Victor Liu <vctl95@gmail.com>
2023-04-04 01:35:25 -0700

    Fix CSS for communityboard.html

--- scripts/communityboard.js
+++ scripts/communityboard.js
@@ -4,22 +4,22 @@ function displayCardsDynamically(collection) {
     let cardTemplate = document.getElementById("listingTemplate");
     console.log("hello")
 
-    db.collection("posts").orderBy("last_updated").get() 
+    db.collection("posts").orderBy("last_updated", "desc").get() 
         .then(allListings => {
             allListings.forEach(doc => { 
                 // var title = doc.data().title;
-                var title = doc.get("title");
-                var image = doc.get("image");
+                let image = doc.get("image");
+                let title = doc.get("title");
+                let category = doc.get("category");
                 console.log("allListings: " + doc.data().title)
 
                 // var description = doc.data().description;
                 var docID = doc.id;
                 let newcard = cardTemplate.content.cloneNode(true);
 
-                newcard.querySelector('.card-title').innerHTML = title;
-                // newcard.querySelector('.card-text').innerHTML = description;
                 newcard.querySelector('.card-image').src = image;
-                // newcard.querySelector('.card-link').href = `./viewListing.html?docID=${docID}`; //Example: ./viewpost.html?NV01
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-category').innerHTML = category;
                 newcard.querySelector('.click-card').setAttribute('onclick', `location.href='./viewListing.html?docID=${docID}'`);
                 document.getElementById("posts-go-here").appendChild(newcard);
 
