0943e1a6f6e351ea8b7aeed24e46a02ca8bd319d
mikaem <mkmanaligod@gmail.com>
2023-04-03 18:18:49 -0700

    added modle

--- scripts/editpost.js
+++ scripts/editpost.js
@@ -5,3 +5,43 @@
 // 4. listen for changes to the form
 // 5. save the changes to Firestore
 // 6. redirect to the post page with the new data
+
+let modal = document.getElementsByClassName("modal");
+let deletebtn = document.getElementsByClassName("deletebtn");
+let modalDelete = document.getElementsByClassName("modaldeleteBtn")
+
+// deletebtn.onclick = function () {
+//     console.log("hello")
+//     modal.style.display = "none";
+//     newcard.querySelector('#modaldeleteBtn').onclick = () => deletePost(doc.id);
+//     console.log("hello")
+// }
+
+deletebtn.onclick(function deletePost(postid) {
+    console.log("hello")
+    var result = confirm("Want to delete?");
+    if (result) {
+        //Logic to delete the item
+        db.collection("posts").doc(postid)
+            .delete()
+            .then(() => {
+                console.log("1. Document deleted from Posts collection");
+                deleteFromMyPosts(postid);
+            }).catch((error) => {
+                console.error("Error removing document: ", error);
+            });
+
+    }
+})
+
+
+
+
+
+
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
--- scripts/myListings.js
+++ scripts/myListings.js
@@ -49,43 +49,57 @@ function displayMyPostCard(doc) {
     newcard.querySelector('.card-image').src = image;
     // newcard.querySelector('.card-description').innerHTML = desc;
     //append to the posts
-    newcard.querySelector('.click-card').setAttribute('onclick', `location.href='./viewListing.html?docID=${docID}'`);
+    // newcard.querySelector('.click-card').setAttribute('onclick', `location.href='./viewListing.html?docID=${docID}'`);
+    // console.log("hello")
+    newcard.querySelector('.card-image').setAttribute('onclick', `location.href='./viewListing.html?docID=${docID}'`);
+    // newcard.querySelector('.deleteBtn').onclick = () => deletePost(doc.id);
+    let delbtn = newcard.querySelector('.deletebttn')
+    delbtn.addEventListener('click', deletePost)
+    console.log("hello")
     document.getElementById("myposts-go-here").prepend(newcard);
+
+}
+
+function deletePost(postid) {
+    // var result = confirm("Want to delete?");
+    // if (result) {
+        //Logic to delete the item
+        db.collection("posts").doc(postid)
+                        .delete()
+        .then(() => {
+            console.log("1. Document deleted from Posts collection");
+            deleteFromMyPosts(postid);
+        }).catch((error) => {
+            console.error("Error removing document: ", error);
+        });
+    // }
 }
 
 
-//function that uploads the listings from the database to the page
-// function displayCardsDynamically(collection) {
-//     let cardTemplate = document.getElementById("postCardTemplate");
-//     console.log("hello")
+function deleteFromMyPosts(postid) {
+    firebase.auth().onAuthStateChanged(user => {
+        db.collection("users").doc(user.uid).update({
+                myposts: firebase.firestore.FieldValue.arrayRemove(postid)
+            })
+            .then(() => {
+                console.log("2. post deleted from user doc");
+                deleteFromStorage(postid);
+            })
+    })
+}
 
-//     db.collection("posts").orderBy("last_updated", "desc").get() //get all docs from collection and orders them by last_updated
-//         .then(allListings => {
-//             // console.log("allListings: " + allListings.docs)
-//             allListings.forEach(doc => { //iterate thru each doc
-//                 var title = doc.data().title;
-//                 console.log("allListings: " + doc.data().title)
 
-//                 var image = doc.data().image;
-//                 console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", image)
-//                 var description = doc.data().description;
-//                 var docID = doc.id;
-//                 let newcard = cardTemplate.content.cloneNode(true);
+function deleteFromStorage(postid) {
+    // Create a reference to the file to delete
+    var imageRef = storageRef.child('images/' + postid + '.jpg');
 
-//                 //update title and text and image etc.
-//                 newcard.querySelector('.card-title').innerHTML = title;
-//                 newcard.querySelector('.card-description').innerHTML = description;
-//                 //update image
-//                 // newcard.querySelector('.card-image').src = `./images/${docID}.jpg`;
-//                 newcard.querySelector('.card-image').src = image;
-//                 // update link
-//                 newcard.querySelector('.card-link').href = `./viewListing.html?docID=${docID}`; //Example: ./viewpost.html?NV01
-
-//                 // //Finally done modifying newcard
-//                 // document.getElementById("posts-go-here").appendChild(newcard);
-//                 document.getElementById("posts-go-here").prepend(newcard);
-
-//             })
-//         })
-// }
-// displayCardsDynamically("posts");
+    // Delete the file
+    imageRef.delete().then(() => {
+        // File deleted successfully
+        console.log("3. image deleted from storage");
+        alert("DELETE is completed!");
+        location.reload();
+    }).catch((error) => {
+        // Uh-oh, an error occurred!
+    });
+}
