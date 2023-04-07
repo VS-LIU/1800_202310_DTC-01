318621a04e8a3a9c7db86e10f8c8b1e6d296ec38
Victor Liu <vctl95@gmail.com>
2023-04-07 13:14:00 -0700

    Fix links

--- scripts/template.js
+++ scripts/template.js
@@ -2,7 +2,7 @@
 // This function loads our custom navbar and footer(icon bar)
 // ----------------------------------------------------------
 function loadSkeleton(){
-    console.log($('#my-navbar').load('./partials/navbar.html'));
-    console.log($('#my-footer').load('./partials/footer.html'));
+    console.log($('#my-navbar').load('../partials/navbar.html'));
+    console.log($('#my-footer').load('../partials/footer.html'));
 }
 loadSkeleton();  //invoke the function
