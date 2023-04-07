8d73b7ec9ddfa537383c2ec497b97ee76ca7dc18
Victor Liu <vctl95@gmail.com>
2023-04-07 12:24:50 -0700

    Add comments to complete template.js

--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,6 @@
-
+// ----------------------------------------------------------
 // This function loads our custom navbar and footer(icon bar)
+// ----------------------------------------------------------
 function loadSkeleton(){
     console.log($('#my-navbar').load('./partials/navbar.html'));
     console.log($('#my-footer').load('./partials/footer.html'));
