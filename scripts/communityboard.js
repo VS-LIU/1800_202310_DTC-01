eaf7490b25758bd02c14b7a9ebde7256a77f5f84
Victor Liu <vctl95@gmail.com>
2023-04-07 12:42:19 -0700

    Clean up code to complete communityboard.js

--- scripts/communityboard.js
+++ scripts/communityboard.js
@@ -1,28 +1,20 @@
-
-//function that uploads the listings from the database to the page
+// Function that populates the page with posts from the database
 function displayCardsDynamically(collection) {
     let cardTemplate = document.getElementById("listingTemplate");
-    console.log("hello")
-
     db.collection("posts").orderBy("last_updated", "desc").get() 
         .then(allListings => {
             allListings.forEach(doc => { 
-                // var title = doc.data().title;
+                var docID = doc.id;
                 let image = doc.get("image");
                 let title = doc.get("title");
                 let category = doc.get("category");
-                console.log("allListings: " + doc.data().title)
-
-                // var description = doc.data().description;
-                var docID = doc.id;
+                
                 let newcard = cardTemplate.content.cloneNode(true);
-
                 newcard.querySelector('.card-image').src = image;
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-category').innerHTML = category;
                 newcard.querySelector('.click-card').setAttribute('onclick', `location.href='./viewListing.html?docID=${docID}'`);
                 document.getElementById("posts-go-here").appendChild(newcard);
-
             })
         })
 }
